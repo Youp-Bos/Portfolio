01d24142b23b0934e23f888f293cb13967fb524c
Youp-Bos <youpbos@tutanota.com>
2021-02-17 16:49:22 +0100

    beginning

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <div class="mainContainer" style="display:flex; justify-content:center; align-items: center; flex-direction: column;">
+        <div class="container" style="display:flex;">
+            <h1>Hello my name is Youp Bos</h1>
+        </div>
+        <div class="containerInfo" style="width: 30%;">
+            <h2>Let me tell you a bit about myself!</h2>
+            <h3>I am in my second year of programming at RocvA/BIT Academy
+                as a software developer. From day one up until now I have been in love with the concept of
+                writing a few lines of code to then see a website start to form from those pieces of code.
+                I love to see myself grow in this field and to achieve something great. </h3>
+            <h2 style="margin-top: .5rem; margin-bottom: .2rem; ">Skills</h2>
+            <div class="containerSkillAsortment" style="display:flex; flex-direction: row;">
+                <div class="containerSkill">
+                    <ul>
+                        <li>PHP</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>MySql</li>
+                    </ul>
+                </div>
+                <div class="containerSkill">
+                    <ul>
+                        <li>OOP</li>
+                    </ul>
+                </div>
+            </div>
+            <h3>Here are some of the <a href="#" style="font-weight: bold;">projects</a> i did</h3>
+            <div class="containerLinks">
+                <ul>
+                    <img src="img/linkedin.png">
+                    <a href="#">LinkedIn</a>
+                    <p></p>
+                    <img src="img/instagram.png">
+                    <a href="#">Instagram</a>
+                    <p></p>
+                    <img src="img/GitHub-Mark-32px.png">
+                    <a href="#">Github</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
