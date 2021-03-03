78d795e46e63938f3f708ae1daeba9ae9eda7977
Youp-Bos <youpbos@tutanota.com>
2021-03-03 14:46:04 +0100

    ...

--- index(old).php
+++ index(old).php
@@ -5,14 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="style.css">
     <title>Portfolio</title>
 </head>
 
 <body>
-    <div class="mainContainer" style="display:flex; justify-content:center; align-items: center; flex-direction: column;">
+    <div class="mainContainer w3-sans-serif" style="display:flex; justify-content:center; align-items: center; flex-direction: column;">
         <div class="container" style="display:flex;">
             <h1>Hello my name is Youp Bos</h1>
         </div>
@@ -22,6 +21,12 @@
                 as a software developer. From day one up until now I have been in love with the concept of
                 writing a few lines of code to then see a website start to form from those pieces of code.
                 I love to see myself grow in this field and to achieve something great. </h3>
+
+            <h2 style="margin-top: .5rem; margin-bottom: .2rem; ">Education</h2>
+
+            <h2 style="margin-top: .5rem; margin-bottom: .2rem; ">Projects</h2>
+            <h3>Here are some of the <a href="#" style="font-weight: bold;">projects</a> i worked on</h3>
+
             <h2 style="margin-top: .5rem; margin-bottom: .2rem; ">Skills</h2>
             <div class="containerSkillAsortment" style="display:flex; flex-direction: row;">
                 <div class="containerSkill">
@@ -35,10 +40,12 @@
                 <div class="containerSkill">
                     <ul>
                         <li>OOP</li>
+                        <li>Bludit</li>
+                        <li>Docker</li>
+                        <li>Bitbucket</li>
                     </ul>
                 </div>
             </div>
-            <h3>Here are some of the <a href="#" style="font-weight: bold;">projects</a> i did</h3>
             <div class="containerLinks">
                 <ul>
                     <img src="img/linkedin.png">
